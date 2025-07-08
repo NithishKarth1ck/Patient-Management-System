6657ff567e068bbe04c32d1bd349ce36eb334233
nishanth-de <nishanthganesh851@gmail.com>
2025-07-08 11:13:04 +0530

    first commit

---
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
@@ -0,0 +1,34 @@
+package com.pm.authservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequestDTO {
+
+    @NotBlank(message = "email should not be blank")
+    @Email(message = "enter a valid email")
+    private String email;
+
+    @NotBlank(message = "password should not be blank")
+    @Size(min = 8,message = "password should have at least 8 characters")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
