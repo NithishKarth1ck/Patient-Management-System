e7d214541c2a79cd3613e564549e2b423c20d62e
nithish <nithishnickzz@gmail.com>
2026-03-21 22:13:33 +0530

    changes in billing service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -2,6 +2,7 @@ package com.pm.billingservice.grpc;
 
 import billing.BillingResponse;
 import billing.BillingServiceGrpc;
+import com.pm.billingservice.service.BillingService;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
@@ -10,17 +11,18 @@ import org.slf4j.LoggerFactory;
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+    private final BillingService billingService;
 
+    public BillingGrpcService(BillingService billingService){
+        this.billingService = billingService;
+    }
     @Override
-    public void createBillingAccount(billing.BillingRequest billingRequest,
+    public void createBillingAccount(billing.BillingRequest request,
     StreamObserver<billing.BillingResponse> responseObserver){
 
-        log.info("createBillingAccount request received {}", billingRequest.toString());
+        log.info("createBillingAccount request received for patientId: {}", request.getPatientId());
 
-        BillingResponse response = BillingResponse.newBuilder()
-                .setAccountId("1234")
-                .setStatus("ACTIVE")
-                .build();
+        BillingResponse response = billingService.createBillingAccount(request);
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
