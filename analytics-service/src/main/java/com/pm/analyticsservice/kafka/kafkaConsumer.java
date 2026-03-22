5474b7409d730a51809b8ce33764f473c72e976e
nithish <nithishnickzz@gmail.com>
2026-03-22 12:23:39 +0530

    update application properties for databes connectivity

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/kafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/kafkaConsumer.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
 
 @Service
-public class KafkaConsumer {
+public class kafkaConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(kafkaConsumer.class);
 
     @KafkaListener(topics="patient",groupId = "analytics-service")
     public void consumeEvent(byte[] event) {
