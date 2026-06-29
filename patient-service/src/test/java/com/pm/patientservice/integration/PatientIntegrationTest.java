5a67111dc80e93871374ec94e5043df530277cee
Nithish Karthick <nithishnickzz@gmail.com>
2026-06-29 23:13:27 +0530

    Remove auto-generated context load test, not runnable without full infrastructure

--- patient-service/src/test/java/com/pm/patientservice/integration/PatientIntegrationTest.java
+++ patient-service/src/test/java/com/pm/patientservice/integration/PatientIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.pm.patientservice;
+package com.pm.patientservice.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pm.patientservice.dto.PatientRequestDTO;
